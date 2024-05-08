c99118ed24189e166acd6b4a89d701947814c222
Tristan Bridges <mophead4@gmail.com>
2024-05-08 15:38:50 -0500

    POST request for data from form works and adds to server, as well as using axios to render to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,56 +1,89 @@
 function onReady() {
-    console.log('Hello from client.js');
+  console.log("Hello from client.js");
 
-    axios({
-        method: 'GET',
-        url: '/artist'
-    })
-        .then(function (response) {
-            // Code that will run on successful response
-            // from the server.
-            console.log(response);
-            // quotesFromServer will be an Array of quotes
-            let quotesFromServer = response.data;
-            let contentDiv = document.getElementById('artistTableBody');
-            for (let artist of quotesFromServer) {
-                contentDiv.innerHTML += `
+  axios({
+    method: "GET",
+    url: "/artist",
+  })
+    .then(function (response) {
+      // Code that will run on successful response
+      // from the server.
+      console.log(response);
+      // quotesFromServer will be an Array of quotes
+      let quotesFromServer = response.data;
+      let contentDiv = document.getElementById("artistTableBody");
+      for (let artist of quotesFromServer) {
+        contentDiv.innerHTML += `
                 <tr>
                     <td>${artist.name}</td>
                     <td>${artist.born}</td>
                     <td>${artist.died}</td>
                 </tr>
             `;
-            }
-        }).catch(function (error) {
-            // Code that will run on any errors from the server.
-            console.log(error);
-            alert('Something bad happened! Check the console for more details.')
-        });
-
-    // TODO Add Axios request for /songs and display on DOM
-
-
-    axios({
-        method: 'GET',
-        url: '/songs'
+      }
     })
-        .then(function (response) {
-            console.log(response);
+    .catch(function (error) {
+      // Code that will run on any errors from the server.
+      console.log(error);
+      alert("Something bad happened! Check the console for more details.");
+    });
 
-            let songsFromServer = response.data;
-            let songsDiv = document.getElementById('songTableBody');
-            for (let song of songsFromServer) {
-                songsDiv.innerHTML += `
+  // TODO Add Axios request for /songs and display on DOM
+
+  axios({
+    method: "GET",
+    url: "/songs",
+  })
+    .then(function (response) {
+      console.log(response);
+
+      let songsFromServer = response.data;
+      let songsDiv = document.getElementById("songTableBody");
+      for (let song of songsFromServer) {
+        songsDiv.innerHTML += `
                 <tr>
                     <td>${song.title}</td>
                     <td>${song.artist}</td>
                 </tr>
                 `;
-            }
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+      alert("ERR MERR GERRD, ERRERR. LERK ERT CERNSERL");
+    });
+}
 
-        }).catch(function (error) {
-            console.log(error);
-            alert('ERR MERR GERRD, ERRERR. LERK ERT CERNSERL')
-        });
-    }
+function addArtist(event) {
+  event.preventDefault();
+
+  let artistToAdd = {};
+  artistToAdd.name = document.getElementById("artistNameInput").value;
+  birthYear = document.getElementById("artistBirthInput").value;
+  artistToAdd.born = Number(birthYear)
+  deathYear = document.getElementById("artistDeathInput").value;
+  artistToAdd.died = Number(deathYear)
+
+  axios({
+    method: "POST",
+    url: "/artist",
+    data: { artistToAdd },
+  })
+    .then((response) => {
+        console.log(artistToAdd)
+
+        let artistDiv = document.getElementById('artistTableBody')
+        artistDiv.innerHTML += `
+        <tr>
+            <td>${artistToAdd.name}</td>
+            <td>${artistToAdd.born}</td>
+            <td>${artistToAdd.died}</td>
+        </tr>
+    `;
+    })
+    .catch((err) => {
+        console.log(err)
+        alert("ERR MERR GERRD, ERRERR. LERK ERT CERNSERL")
+    })
+}
 onReady();
--- server/server.js
+++ server/server.js
@@ -1,62 +1,78 @@
-const express = require('express');
+const bodyParser = require("body-parser");
+const express = require("express");
 
 const app = express();
 const PORT = 5001;
 
 const artistListArray = [
-    {
-        name: 'Miles Davis',
-        born: 1926,
-        died: 1990,
-    },
-    {
-        name: 'Duke Ellington',
-        born: 1899,
-        died: 1974,
-    },
-    {
-        name: 'John Coltrane',
-        born: 1926,
-        died: 1987,
-    },
-    {
-        name: 'Louis Daniel Armstrong',
-        born: 1901,
-        died: 1971,
-    },
+  {
+    name: "Miles Davis",
+    born: 1926,
+    died: 1990,
+  },
+  {
+    name: "Duke Ellington",
+    born: 1899,
+    died: 1974,
+  },
+  {
+    name: "John Coltrane",
+    born: 1926,
+    died: 1987,
+  },
+  {
+    name: "Louis Daniel Armstrong",
+    born: 1901,
+    died: 1971,
+  },
 ];
 
 const songListArray = [
-    {
-        title: 'Take Five',
-        artist: 'The Dave Brubeck Quartet',
-    },
-    {
-        title: 'So What',
-        artist: 'Miles Davis',
-    },
-    {
-        title: 'Sing Sing Sing',
-        artist: 'Benny Goodman',
-    },
-    {
-        title: 'Take the "A" Train',
-        artist: 'The Dave Brubeck Quartet',
-    },
+  {
+    title: "Take Five",
+    artist: "The Dave Brubeck Quartet",
+  },
+  {
+    title: "So What",
+    artist: "Miles Davis",
+  },
+  {
+    title: "Sing Sing Sing",
+    artist: "Benny Goodman",
+  },
+  {
+    title: 'Take the "A" Train',
+    artist: "The Dave Brubeck Quartet",
+  },
 ];
 
-app.use(express.static('server/public'));
+app.use(express.static("server/public"));
+app.use(express.json());
+// ! for postman 
+    //use app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/artist', (req, res) => {
-    res.send(artistListArray);
+app.get("/artist", (req, res) => {
+  res.send(artistListArray);
+});
+
+app.post("/artist", (req, res) => {
+  console.log(`Get a POST request!`, req.body);
+
+  let artist = req.body.artistToAdd;
+
+  console.log("Adding new artist:", artist);
+  artistListArray.push(artist);
+
+  res.sendStatus(201);
 });
 
 // TODO - Add GET for songs
 
-app.get('/songs', (req, res)=>{
-    res.send(songListArray)
-})
+app.get("/songs", (req, res) => {
+  res.send(songListArray);
+});
 
 app.listen(PORT, () => {
-    console.log('listening on port', PORT)
+  console.log("listening on port", PORT);
 });
